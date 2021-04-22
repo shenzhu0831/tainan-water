854f607c6767fa017a53dbd32e4103d53e565a13
Cathy <shenzhu0831@gmail.com>
2021-04-23 03:47:11 +0800

    完成 popover 功能

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+const waterRegimeInfo = document.querySelector(".water_regime_info")
+const popoverCloseButton = document.querySelector(".popover_header_close")
+const popoverCard = document.querySelector(".popover")
+const popoverBackground = document.querySelector(".popover_background")
+
+// 接全台水庫 Data
 async function getTainanReservoirData() {
   const response = await fetch("./js/open-data/tainan-reservoir-data.json");
   return response.json();
@@ -15,9 +21,28 @@ async function getReservoir() {
     return Math.round(accumulator + reservoir.FullWaterLevel);
   }, 0);
 
-  const x = (effectiveCapacity / FullWaterLevel).toFixed(2)
+  const reservoirRegimeData = (effectiveCapacity / FullWaterLevel).toFixed(2)
 
-  reservoirRegimeValue.innerText = `${x}%`
+  reservoirRegimeValue.innerText = `${reservoirRegimeData}%`
+}
+
+// 目前水情元件效果
+function popoverOpen(event){
+  if(event.target.closest(".water_regime_info")){
+    popoverCard.classList.add("open")
+    popoverBackground.classList.add("open")
+  }
+}
+
+function popoverClose(event){
+  if(event.target){
+    popoverCard.classList.remove("open")
+    popoverBackground.classList.remove("open")
+  }
 }
 
 getReservoir()
+
+waterRegimeInfo.addEventListener("click", popoverOpen)
+popoverCloseButton.addEventListener("click", popoverClose)
+popoverBackground.addEventListener("click", popoverClose)
